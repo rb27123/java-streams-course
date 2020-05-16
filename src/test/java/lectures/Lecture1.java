65fc8205c2214f0cf988bd98ead9b9404c2cd270
Rodrigo Bejarano <rbejarano@mcclatchyservices.com>
2020-05-16 13:43:01 -0500

    First 2 lectures

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -1,33 +1,44 @@
 package lectures;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import beans.Person;
-
 import com.google.common.collect.ImmutableList;
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import mockdata.MockData;
-import org.assertj.core.util.Lists;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Lecture1 {
 
-  @Test
-  public void imperativeApproach() throws IOException {
-    List<Person> people = MockData.getPeople();
-    // 1. Find people aged less or equal 18
-    // 2. Then change implementation to find first 10 people
-    //XDDDDDDDD
+    @Test
+    public void imperativeApproach() throws IOException {
+        List<Person> people = MockData.getPeople();
+        // 1. Find people aged less or equal 18
+        // 2. Then change implementation to find first 10 people
+        List<Person> people1 = new ArrayList<>();
+        int counter = 0;
+        for (Person person : people) {
+            if (counter < 10) {
+                if (person.getAge() <= 18) {
+                    people1.add(person);
+                    counter++;
+                }
+            } else break;
+        }
+        for (Person person : people1) {
+            System.out.println(person.getAge());
+        }
+    }
 
-  }
-
-  @Test
-  public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
-  }
+    @Test
+    public void declarativeApproachUsingStreams() throws Exception {
+        ImmutableList<Person> people = MockData.getPeople();
+        Predicate<Person> personPredicate = person -> person.getAge() <= 18;
+        people.stream()
+                .filter(personPredicate)
+                .limit(10)
+                .forEach(System.out::println);
+    }
 }
