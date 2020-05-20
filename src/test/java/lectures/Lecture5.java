56a2ea13733eb639c5d9f1ff51ab565f073e8c7a
Rodrigo Bejarano <rbejarano@mcclatchyservices.com>
2020-05-19 21:04:06 -0500

    Lectures: 3,4,5 and 6

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -7,37 +7,53 @@ import beans.Car;
 import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture5 {
 
-  @Test
-  public void understandingFilter() throws Exception {
-    ImmutableList<Car> cars = MockData.getCars();
+    @Test
+    public void understandingFilter() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
+        List<Car> carsFiltered = cars.stream()
+                .filter(car -> car.getPrice() < 10000)
+                .collect(Collectors.toList());
+        carsFiltered.forEach(System.out::println);
+        System.out.println(carsFiltered.size());
+    }
 
-  }
+    @Test
+    public void ourFirstMapping() throws Exception {
+        // transform from one data type to another
+        List<Person> people = MockData.getPeople();
+        List<PersonDTO> dtos = people.stream()
+                .map(PersonDTO::map)
+                .collect(Collectors.toList());
+        dtos.forEach(System.out::println);
+        assertThat(dtos).hasSize(1000);
+    }
 
-  @Test
-  public void ourFirstMapping() throws Exception {
-    // transform from one data type to another
-    List<Person> people = MockData.getPeople();
+    @Test
+    public void averageCarPrice() throws Exception {
+        // calculate average of car prices
+        double average = MockData.getCars()
+                .stream()
+                .mapToDouble(Car::getPrice)
+                .average()
+                .orElse(0);
+        System.out.println(average);
+    }
 
-  }
+    @Test
+    public void test() throws Exception {
 
-  @Test
-  public void averageCarPrice() throws Exception {
-    // calculate average of car prices
-
-  }
-
-  @Test
-  public void test() throws Exception {
-
-  }
+    }
 }
 
 
